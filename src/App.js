7fe57993c56e88e36528eaff18ecf95335df19b4
admhabits <admhabits@gmail.com>
2021-07-15 09:21:37 +0700

    Konfigurasi Routing Pertama

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import React, { Component, StrictMode } from 'react';
-
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <StrictMode>
-          <>
-            
-          </>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path={"/"} component={()=>(
+                  <><h2>Ini Landing Page</h2></>
+                )}/>
+
+              <Switch>
+                  {/*NESTING ROUTER CLIENT START*/}
+                  <Route path={"/client"} component={()=>(
+                    
+                      <Switch>
+                          <Route exact path={"/client/posts"} render={()=>(
+                                <><h2>Client Posts</h2></>
+                            )}/>
+                          <Route exact path={"/client/profile"} render={()=>(
+                                <><h2>Client Profile</h2></>
+                            )}/>
+                          <Route render={()=>(
+                              <>Not Found 404</>
+                            )}/>
+                      </Switch>
+
+                    )}/>
+                  {/*NESTING ROUTER CLIENT END*/}
+
+                  {/*NESTING ROUTER ADMIN START*/}
+                  <Route path={"/admin"} component={()=>(
+
+                      <Switch>
+                          <Route exact path={"/admin/dashboard"} render={()=>(
+                                <><h2>Admin Dashboard</h2></>
+                            )}/>
+                          <Route exact path={"/admin/settings"} render={()=>(
+                                <><h2>Admin Settings</h2></>
+                            )}/>
+                          <Route render={()=>(
+                              <>Not Found 404</>
+                            )}/>
+                      </Switch>
+
+                    )}/>
+                  {/*NESTING ROUTER ADMIN END*/}
+                  
+                  <Route render={()=>(
+                      <>Not Found 404</>
+                    )}/>
+              </Switch>
+             
+
+              <Route path={"/*"} render={()=>(
+
+                  <>Not Found 404</>
+
+                )}/>
+            </Switch>
+          </BrowserRouter>
       </StrictMode>
     );
   }
