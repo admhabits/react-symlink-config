22a42d00a211c210601b0a2313e6b9586010b044
admhabits <admhabits@gmail.com>
2021-07-15 08:36:36 +0700

    Upload Files Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component, StrictMode } from 'react';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <StrictMode>
+          <>
+            
+          </>
+      </StrictMode>
+    );
+  }
+}
